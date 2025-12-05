9b83cbd59a796079c9c566902360bacc8410e7d9
hachi029 <hachi029@163.com>
2025-12-05 21:38:40 +0800

    update

--- lib/ngx/process.lua
+++ lib/ngx/process.lua
@@ -17,15 +17,18 @@ end
 
 local ngx_phase = ngx.get_phase
 
+-- OpenResty 里的进程分为如下六种类型：
 local process_type_names = {
-    [0 ]  = "single",
-    [1 ]  = "master",
-    [2 ]  = "signaller",
-    [3 ]  = "worker",
-    [4 ]  = "helper",
-    [99]  = "privileged agent",
+    [0 ]  = "single",           --单一进程，即非 master/worker 模式；
+    [1 ]  = "master",           --监控进程，即 master 进程；
+    [2 ]  = "signaller",        --信号进程，即 “-s” 参数时的进程；
+    [3 ]  = "worker",           --工作进程，最常用的进程，对外提供服务；
+    [4 ]  = "helper",           --辅助进程，不对外提供服务，例如 cache 进程；
+    [99]  = "privileged agent", --特权进程，OpenResty 独有的进程类型。
 }
-
+---关闭了所有的监听端口，特权进程不能接受请求，“rewrite_by_lua” “access_by_lua” “content_by_lua” “log_by_lua” 等请求处理相关的执行阶段没有意义，这些阶段里的代码在特权进程里都不会运行。
+-- 但有一个阶段是它可以使用的，那就是 “init_worker_by_lua”，特权进程要做的工作就是 ngx.timer.* 启动若干个定时器，运行周期任务，
+-- 通过共享内存等方式与其他 worker 进程通信，利用自己的 root 权限做其他 worker 进程想做而不能做的工作
 
 local C = ffi.C
 local _M = { version = base.version }
